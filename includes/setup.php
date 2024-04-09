4977ee19ea6c9df0bc5eb967642a8369f867b9b1
Waleed Al-Shaikhli <32021897+al-shaikhli-w@users.noreply.github.com>
2024-04-09 12:32:10 +0200

    Update CSS and SCSS files for navbar styling, add new block type for ourClients, fix bug in login functionality, and update code formatting

--- includes/setup.php
+++ includes/setup.php
@@ -29,3 +29,12 @@ if (!function_exists('dch_register_menus')) {
 
 
 add_theme_support('post-thumbnails');
+
+// Support  svg file to import it to the media files.
+
+function allow_svg_upload($mimes)
+{
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'allow_svg_upload');
