0a1403312a93ee4dd0117974ea3431313f40cd29
Waleed Al-Shaikhli <32021897+al-shaikhli-w@users.noreply.github.com>
2024-04-16 01:54:46 +0300

    Add new block type for prices-sections and update CSS and SCSS files for navbar styling

---
+++ blocks/prices-sections/template.php
@@ -0,0 +1,50 @@
+<div class="max-w-sm mx-auto grid gap-6 lg:grid-cols-3 items-start lg:max-w-none">
+
+	<!-- Pricing tab 1 -->
+	<article class="h-full">
+		<div class="relative text-white flex flex-col h-full p-6 rounded-2xl bg-balck border border-white shadow">
+			<div class="mb-5">
+				<h3 class="font-bold font-BespokeStencil text-4xl mb-1 text-center">Personal</h3>
+				<div class="inline-flex items-baseline mb-2 text-center">
+					<span class="font-bold text-3xl">$</span>
+					<span class="font-bold text-4xl">35</span>
+					<span class="font-medium">/mo</span>
+				</div>
+				<div class="text-sm mb-5">There are many variations available, but the majority have suffered.</div>
+				<a href="#" class="btn btn--primary w-full text-center font-helvetica font-bold">
+					Purchase Plan
+				</a>
+			</div>
+			<h4 class="font-medium mb-3">Includes:</h4>
+			<ul class="text-sm space-y-3 grow">
+				<li class="flex items-center">
+					<div class="icon bg-white">
+						<svg class="w-3 h-3 fill-black mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+							<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+						</svg>
+					</div>
+					<span>Unlimited placeholder texts</span>
+				</li>
+				<li class="flex items-center">
+					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+					</svg>
+					<span>Consectetur adipiscing elit</span>
+				</li>
+				<li class="flex items-center">
+					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+					</svg>
+					<span>Excepteur sint occaecat cupidatat</span>
+				</li>
+				<li class="flex items-center">
+					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+					</svg>
+					<span>Officia deserunt mollit anim</span>
+				</li>
+			</ul>
+		</div>
+	</article>
+
+</div>
--- blocks/register_blocks.php
+++ blocks/register_blocks.php
@@ -3,3 +3,4 @@ register_block_type(get_template_directory() . '/blocks/hero/block.json');
 register_block_type(get_template_directory() . '/blocks/ourClients/block.json');
 register_block_type(get_template_directory() . '/blocks/work/block.json');
 register_block_type(get_template_directory() . '/blocks/whyUsCard/block.json');
+register_block_type(get_template_directory() . '/blocks/prices-sections/block.json');
