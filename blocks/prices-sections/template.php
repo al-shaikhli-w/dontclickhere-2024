f07984ae7226a9a4745cacfdcd6b4796cfe29945
Waleed Al-Shaikhli <32021897+al-shaikhli-w@users.noreply.github.com>
2024-04-21 04:12:54 +0300

    finished the quastions sections

--- blocks/prices-sections/template.php
+++ blocks/prices-sections/template.php
@@ -1,50 +1,55 @@
-<div class="max-w-sm mx-auto grid gap-6 lg:grid-cols-3 items-start lg:max-w-none">
-
-	<!-- Pricing tab 1 -->
-	<article class="h-full">
-		<div class="relative text-white flex flex-col h-full p-6 rounded-2xl bg-balck border border-white shadow">
-			<div class="mb-5">
-				<h3 class="font-bold font-BespokeStencil text-4xl mb-1 text-center">Personal</h3>
-				<div class="inline-flex items-baseline mb-2 text-center">
-					<span class="font-bold text-3xl">$</span>
-					<span class="font-bold text-4xl">35</span>
-					<span class="font-medium">/mo</span>
+<?php
+// Check rows exists.
+if (have_rows('pricing')) :
+?>
+	<section class="pricing-cards">
+		<?php
+		while (have_rows('pricing')) : the_row();
+			$popular = get_sub_field('popular');
+			$heading = get_sub_field('heading');
+			$paragraph = get_sub_field('paragraph');
+			$euro = get_sub_field('euro'); ?>
+			<article class="pricing-card <?php echo $popular ? "popular-card" : "" ?>">
+				<div class="pricing-card-header">
+					<h3 class="pricing-card-header__heading"><?php echo $heading; ?></h3>
+					<p class="pricing-card-header__paragraph"><?php echo $paragraph; ?></p>
+					<div class="pricing-card-header__pricing">
+						<span class="text-xl relative -top-3 mr-2">* ab €</span>
+						<span class="font-bold text-4xl"><?php echo $euro; ?></span>
+						<span class="font-medium text-lg">00</span>
+						<span class="mx-1">/</span>
+						<span class="font-thin">einmalig</span>
+					</div>
+					<hr>
 				</div>
-				<div class="text-sm mb-5">There are many variations available, but the majority have suffered.</div>
-				<a href="#" class="btn btn--primary w-full text-center font-helvetica font-bold">
+				<div class="includes">
+					<?php
+					if (have_rows('items_content')) : ?>
+						<ul class="includes__lists">
+							<?php while (have_rows('items_content')) : the_row();
+								$item_content = get_sub_field('item_content');
+							?>
+								<li class="includes__lists__item">
+									<div class="icon">
+										<svg width="8px" height="8px" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+											<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+										</svg>
+									</div>
+									<span class="includes__lists__item__content"><?php echo $item_content ?></span>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					<?php endif; ?>
+				</div>
+				<a href="#" class="pricing-card-header__cta">
 					Purchase Plan
 				</a>
-			</div>
-			<h4 class="font-medium mb-3">Includes:</h4>
-			<ul class="text-sm space-y-3 grow">
-				<li class="flex items-center">
-					<div class="icon bg-white">
-						<svg class="w-3 h-3 fill-black mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-							<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-						</svg>
-					</div>
-					<span>Unlimited placeholder texts</span>
-				</li>
-				<li class="flex items-center">
-					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-					</svg>
-					<span>Consectetur adipiscing elit</span>
-				</li>
-				<li class="flex items-center">
-					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-					</svg>
-					<span>Excepteur sint occaecat cupidatat</span>
-				</li>
-				<li class="flex items-center">
-					<svg class="w-3 h-3 fill-white mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-						<path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-					</svg>
-					<span>Officia deserunt mollit anim</span>
-				</li>
-			</ul>
-		</div>
-	</article>
-
-</div>
+				<span class="light-border"></span>
+				<?php
+				if ($popular) { ?>
+					<span class="popular">The most popular</span>
+				<?php } ?>
+			</article>
+	<?php endwhile;
+	endif; ?>
+	</section>
