69ce0ff835199e74adf3f9a242d1b395f2cc9e20
waleed <waleedkaleed24@gmail.com>
2024-10-11 09:40:28 +0200

    refactor: Remove unused files and update asset paths

--- assets/public/src/js/animation/singlePost.js
+++
@@ -1,8 +0,0 @@
-// import { gsap } from "gsap";
-// import ScrollTrigger from 'gsap/ScrollTrigger';
-// gsap.registerPlugin(ScrollTrigger);
-// // start Single post animation+
-// window.addEventListener("DOMContentLoaded", () => {
-
-
-// });
--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -4,13 +4,13 @@ function dch_enqueue()
 {
 	wp_register_style(
 		'dch_theme',
-		get_theme_file_uri('assets/public/dist/css/main.css')
+		get_theme_file_uri('assets/dist/css/main.css')
 	);
 
 	wp_enqueue_style('dch_theme');
 	wp_register_script(
 		'dch_theme',
-		get_theme_file_uri('assets/public/dist/js/main.js'),
+		get_theme_file_uri('assets/dist/js/main.js'),
 		[],
 		false,
 		true
